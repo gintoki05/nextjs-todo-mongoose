f7dbef327a330c1b65b21fb8c49c9e91b9fe95e9
mustechs <musitechnicalsolution@gmail.com>
2021-07-25 20:44:23 +0700

    change url

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/header';
 import AddTodo from '../containers/addTodo';
 import TodoList from '../containers/todoList';
 import axios from 'axios';
+import { NEXT_URL } from '../config/index';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,7 +13,7 @@ export default function Home() {
   useEffect(async () => {
     setIsLoading(true);
 
-    const result = await axios.get('http://localhost:3000/api/todo');
+    const result = await axios.get(`${NEXT_URL}/api/todo`);
     setTodos(result.data.data);
 
     setIsLoading(false);
@@ -20,7 +21,7 @@ export default function Home() {
 
   const addTodo = async (todoText) => {
     if (todoText && todoText.length > 0) {
-      const result = await axios.post('http://localhost:3000/api/todo', {
+      const result = await axios.post(`${NEXT_URL}/api/todo`, {
         todoText,
       });
       setTodos([...todos, result?.data.data]);
@@ -30,12 +31,9 @@ export default function Home() {
   const editTodoItem = async (todo) => {
     const newTodoText = prompt('Enter new todo text or description:');
     if (newTodoText != null) {
-      const result = await axios.put(
-        'http://localhost:3000/api/todo/' + todo._id,
-        {
-          todoText: newTodoText,
-        }
-      );
+      const result = await axios.put(`${NEXT_URL}/api/todo` + todo._id, {
+        todoText: newTodoText,
+      });
       const moddedTodos = todos.map((_todo) => {
         if (_todo._id === todo._id) {
           return result?.data.data;
@@ -49,7 +47,7 @@ export default function Home() {
 
   const deleteTodoItem = async (todo) => {
     if (confirm('Do you really want to delete this item?')) {
-      await axios.delete('http://localhost:3000/api/todo/' + todo._id);
+      await axios.delete(`${NEXT_URL}/api/todo` + todo._id);
       const newTodos = todos.filter((_todo) => _todo._id !== todo._id);
       setTodos(newTodos);
     }
